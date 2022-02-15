a2431ab709d9d882600c78f33b8b88c2a920ef77
lonewolf58 <africawolf58@gmail.com>
2022-02-15 08:30:36 +0100

    Initial Commit

---
+++ src/main/java/com/marshall/mccourse/item/ModItems.java
@@ -0,0 +1,214 @@
+package com.marshall.mccourse.item;
+
+import com.marshall.mccourse.MCCourseMod;
+import com.marshall.mccourse.util.Registration;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+
+public class ModItems
+{
+    public static final RegistryObject<Item> COPPER_INGOT =
+            Registration.ITEMS.register("copper_ingot",
+                    () -> new Item(new Item.Properties().group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_WIRE =
+            Registration.ITEMS.register("copper_wire",
+                    () -> new Item(new Item.Properties().group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPERED_APPLE =
+            Registration.ITEMS.register("coppered_apple",
+                   () -> new CopperedApple());
+
+    /* TOOLS */
+
+    public static final RegistryObject<Item> COPPER_SHOVEL =
+            Registration.ITEMS.register("copper_shovel",
+                    () -> new ShovelItem(ModItemTier.COPPER, 0f, 0f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(150)
+                                    .addToolType(ToolType.SHOVEL, 2)
+                                    .group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_HOE =
+            Registration.ITEMS.register("copper_hoe",
+                    () -> new HoeItem(ModItemTier.COPPER, 0, 0f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(150)
+                                    .addToolType(ToolType.HOE, 2)
+                                    .group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_PICKAXE =
+            Registration.ITEMS.register("copper_pickaxe",
+                    () -> new PickaxeItem(ModItemTier.COPPER, 0, 0f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(150)
+                                    .addToolType(ToolType.PICKAXE, 2)
+                                    .group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_SWORD =
+            Registration.ITEMS.register("copper_sword",
+                    () -> new SwordItem(ModItemTier.COPPER, 20, 0f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(150)
+                                    .group(MCCourseMod.COURSE_TAB)));
+
+    /* ARMOR */
+
+    public static final RegistryObject<Item> COPPER_HELMET =
+            Registration.ITEMS.register("copper_helmet",
+                    () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD,
+                            new Item.Properties().group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_CHESTPLATE =
+            Registration.ITEMS.register("copper_chestplate",
+                    () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST,
+                            new Item.Properties().group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_LEGGINGS =
+            Registration.ITEMS.register("copper_leggings",
+                    () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS,
+                            new Item.Properties().group(MCCourseMod.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COPPER_BOOTS =
+            Registration.ITEMS.register("copper_boots",
+                    () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET,
+                            new Item.Properties().group(MCCourseMod.COURSE_TAB)));
+
+
+
+    public static void register() { }
+
+    public enum ModArmorMaterial implements IArmorMaterial
+    {
+        COPPER(50, new int[] { 3, 6, 4, 3 }, 1, SoundEvents.ITEM_ARMOR_EQUIP_IRON,
+                Ingredient.fromStacks(new ItemStack(ModItems.COPPER_INGOT.get())),
+                MCCourseMod.MOD_ID + ":copper", 0, 0.1f);
+
+        private final int durability;
+        private final int[] damageReductionAmountArray;
+        private final int enchantability;
+        private final SoundEvent soundEvent;
+        private final Ingredient repairMaterial;
+        private final String name;
+        private final float toughness;
+        private final float knockbackResistance;
+
+        ModArmorMaterial(int durability, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent,
+                         Ingredient repairMaterial, String name, float toughness, float knockbackResistance)
+        {
+            this.durability = durability;
+            this.damageReductionAmountArray = damageReductionAmountArray;
+            this.enchantability = enchantability;
+            this.soundEvent = soundEvent;
+            this.repairMaterial = repairMaterial;
+            this.name = name;
+            this.toughness = toughness;
+            this.knockbackResistance = knockbackResistance;
+        }
+
+        @Override
+        public int getDurability(EquipmentSlotType slotIn)
+        {
+            return this.durability;
+        }
+
+        @Override
+        public int getDamageReductionAmount(EquipmentSlotType slotIn)
+        {
+            return damageReductionAmountArray[slotIn.getIndex()];
+        }
+
+        @Override
+        public int getEnchantability()
+        {
+            return this.enchantability;
+        }
+
+        @Override
+        public SoundEvent getSoundEvent()
+        {
+            return this.soundEvent;
+        }
+
+        @Override
+        public Ingredient getRepairMaterial()
+        {
+            return this.repairMaterial;
+        }
+
+        @Override
+        public String getName()
+        {
+            return this.name;
+        }
+
+        @Override
+        public float getToughness()
+        {
+            return this.toughness;
+        }
+
+        @Override
+        public float getKnockbackResistance() {
+            return knockbackResistance;
+        }
+    }
+    public enum ModItemTier implements IItemTier
+    {
+        COPPER(2, 150, 2.5f, 0f, 15,
+              Ingredient.fromStacks(new ItemStack(ModItems.COPPER_INGOT.get())));
+
+        private final int harvestLevel;
+        private final int maxUses;
+        private final float efficiency;
+        private final float attackDamage;
+        private final int enchantability;
+        private final Ingredient repairMaterial;
+
+        ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Ingredient repairMaterial) {
+            this.harvestLevel = harvestLevel;
+            this.maxUses = maxUses;
+            this.efficiency = efficiency;
+            this.attackDamage = attackDamage;
+            this.enchantability = enchantability;
+            this.repairMaterial = repairMaterial;
+        }
+
+
+        @Override
+        public int getMaxUses() {
+            return maxUses;
+        }
+
+        @Override
+        public float getEfficiency() {
+            return efficiency;
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return attackDamage;
+        }
+
+        @Override
+        public int getHarvestLevel() {
+            return harvestLevel;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return enchantability;
+        }
+
+        @Override
+        public Ingredient getRepairMaterial() {
+            return repairMaterial;
+        }
+    }
+
+}
